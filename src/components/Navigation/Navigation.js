d5c1a511b2d0549132a9cb1b623108c47bb765c4
Paul Billings <paulbillings101@yahoo.co.uk>
2019-10-25 16:53:19 +0100

    added logo and increased font size og login and register

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,20 +6,34 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Container from '@material-ui/core/Container';
+import emojiLogo from "./emojiLogo.png";
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   title: {
-     
+    textAlign: "left",
+    maxWidth: "50%",
   },
   loginDiv: {
   	marginLeft: "auto",
   },
   login: {
   	marginRight: "1rem",
+  	fontSize: "1.5em",
   },
+  register: {
+  	fontSize: "1.5em",
+  },
+  logo: {
+  	maxWidth: "30%",
+  	height: "auto",
+  	borderRadius: "50%",
+   	border: "1px solid black",
+   	boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  }
 });
 
   const Navigation = ({ classes, theme }) => {
@@ -27,12 +41,12 @@ const styles = theme => ({
         <div className={classes.root}>
         <AppBar position="static">
             <Toolbar>
-                <Typography className={classes.title} variant="h3" color="inherit">
-                Emoji Face
-                </Typography>
+                <div className={classes.title} color="inherit">
+                	<img className={classes.logo} src={emojiLogo} />
+                </div>
                 <div className={classes.loginDiv}>
                 	<Button className={classes.login} color="inherit">Login</Button>
-                	<Button color="inherit">Register</Button>
+                	<Button className={classes.register} color="inherit">Register</Button>
                 </div>
             </Toolbar>
         </AppBar>
